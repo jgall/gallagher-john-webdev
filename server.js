b0f8443ee0b845efdbee4761ce868e571531b599
Gallagher <jggll@amazon.com>
2017-07-03 09:37:00 -0400

    Add env variable for MLAB access url and change logging strategy.

--- server.js
+++ server.js
@@ -1,15 +1,28 @@
+// initialize custom logger.
+var logger = console.log;
+if (process.env.MLAB_USERNAME_WEBDEV) {
+    var logger = function(){};
+}
+
+logger("Starting App...");
+
 var express = require('express');
 var app = express();
+logger("Express acquired...");
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+logger("Body Parser acquired...");
 
 // configure a public directory to host static content
 app.use(express.static(__dirname + '/public'));
+logger("Public Directory initialized...");
 
 require ("./test/app.js")(app);
+logger("Main app.js run...");
 
 var port = process.env.PORT || 3000;
 
 app.listen(port);
+logger("Listening...");
