817a716c502ab343ef4111a26d735d80ca2666f3
Gallagher <jggll@amazon.com>
2017-07-30 13:08:51 -0400

    Add closures around spec testing files.

--- test/test.js
+++ test/test.js
@@ -1,29 +1,31 @@
-'use strict';
+(function() {
+    'use strict';
 
-let assert = require('assert');
-let request = require('supertest');
-let describe = require("mocha").describe;
-let beforeEach = require("mocha").beforeEach;
-let afterEach = require("mocha").afterEach;
-describe('Initialize Tests', function () {
-    it('should always get run and should always pass: ', function () {
-        assert.equal(-1, [1, 2, 3].indexOf(4));
+    let assert = require('assert');
+    let request = require('supertest');
+    let describe = require("mocha").describe;
+    let beforeEach = require("mocha").beforeEach;
+    let afterEach = require("mocha").afterEach;
+    describe('Initialize Tests', function () {
+        it('should always get run and should always pass: ', function () {
+            assert.equal(-1, [1, 2, 3].indexOf(4));
+        });
     });
-});
 
-describe("Normal Server Functionality", function () {
-    let server;
-    beforeEach(function () {
-        server = require("../server");
+    describe("Normal Server Functionality", function () {
+        let server;
+        beforeEach(function () {
+            server = require("../server");
+        });
+        afterEach(function () {
+            server.close();
+        });
+        it('responds to /', function testSlash(done) {
+            request(server)
+                .get('/')
+                .expect(200, done);
+        });
     });
-    afterEach(function () {
-        server.close();
-    });
-    it('responds to /', function testSlash(done) {
-        request(server)
-            .get('/')
-            .expect(200, done);
-    });
-});
+})();
 
 
--- test/website.service.server.spec.js
+++ test/website.service.server.spec.js
@@ -1,24 +1,16 @@
 /**
  * Created by jggll on 7/23/17.
  */
-'use strict';
+(function() {
+    'use strict';
 
-let assert = require('assert');
-let request = require('supertest');
-let describe = require("mocha").describe;
-let beforeEach = require("mocha").beforeEach;
-let afterEach = require("mocha").afterEach;
+    let assert = require('assert');
+    let request = require('supertest');
+    let describe = require("mocha").describe;
+    let beforeEach = require("mocha").beforeEach;
+    let afterEach = require("mocha").afterEach;
 
-describe("Website Service", function () {
-    let server;
-    beforeEach(function () {
-        server = require("../server");
-    });
-    afterEach(function () {
-        server.close();
-    });
-
-    describe('finding websites by id', function() {
+    describe("Website Service", function () {
         let server;
         beforeEach(function () {
             server = require("../server");
@@ -26,73 +18,83 @@ describe("Website Service", function () {
         afterEach(function () {
             server.close();
         });
-        it('finds a website by id', function testFindWebsiteById(done) {
+
+        describe('finding websites by id', function() {
+            let server;
+            beforeEach(function () {
+                server = require("../server");
+            });
+            afterEach(function () {
+                server.close();
+            });
+            it('finds a website by id', function testFindWebsiteById(done) {
+                request(server)
+                    .get('/api/website/678')
+                    .expect(200)
+                    .end((err, res) => {
+                        assert.deepEqual(res.body, {"_id": "678", "name": "Checkers", "developerId": "123", "description": "Lorem"});
+                        done();
+                    });
+            });
+
+            it('404s on finding websites that do not exist', function testFindNonExistentWebsite(done) {
+                request(server)
+                    .get('/api/website/241523')
+                    .expect(404, done);
+            });
+        });
+
+        it('finds websites for user', function testGetWebsitesForUser(done) {
             request(server)
-                .get('/api/website/678')
+                .get('/api/user/123/website')
                 .expect(200)
                 .end((err, res) => {
-                    assert.deepEqual(res.body, {"_id": "678", "name": "Checkers", "developerId": "123", "description": "Lorem"});
+                    assert.equal(res.body.length, 3);
+                    assert.deepEqual(res.body[0], {
+                        "_id": "890",
+                        "name": "Go",
+                        "developerId": "123",
+                        "description": "Lorem"
+                    });
                     done();
                 });
         });
 
-        it('404s on finding websites that do not exist', function testFindNonExistentWebsite(done) {
+        it('creates a website', function testCreateWebsite(done) {
             request(server)
-                .get('/api/website/241523')
-                .expect(404, done);
-        });
-    });
-
-    it('finds websites for user', function testGetWebsitesForUser(done) {
-        request(server)
-            .get('/api/user/123/website')
-            .expect(200)
-            .end((err, res) => {
-                assert.equal(res.body.length, 3);
-                assert.deepEqual(res.body[0], {
-                    "_id": "890",
-                    "name": "Go",
-                    "developerId": "123",
-                    "description": "Lorem"
-                });
-                done();
-            });
-    });
-
-    it('creates a website', function testCreateWebsite(done) {
-        request(server)
-            .post('/api/user/123/website')
-            .send({"name": "testWebsite1", "developerId": "123", "description": "Lorem"})
-            .expect(200)
-            .end((err, res) => {
-                assert.equal(res.body.name, "testWebsite1");
-                request(server)
-                    .get('/api/user/123/website')
-                    .expect(200)
-                    .end((err, res) => {
-                        assert.equal(res.body[3].name, "testWebsite1");
-                        done();
-                    });
-            });
-    });
-
-    it('updates a website', function testUpdateWebsite(done) {
-        request(server).put('/api/website/123')
-            .send({"_id": "123", "name": "Facebook2", "developerId": "456", "description": "Lorem"})
-            .expect(200)
-            .end((err, res) => {
-                request(server)
-                    .get('/api/website/123')
-                    .expect(200)
-                    .end((err, res) => {
-                        assert.deepEqual(res.body, {
-                            "_id": "123",
-                            "name": "Facebook2",
-                            "developerId": "456",
-                            "description": "Lorem"
+                .post('/api/user/123/website')
+                .send({"name": "testWebsite1", "developerId": "123", "description": "Lorem"})
+                .expect(200)
+                .end((err, res) => {
+                    assert.equal(res.body.name, "testWebsite1");
+                    request(server)
+                        .get('/api/user/123/website')
+                        .expect(200)
+                        .end((err, res) => {
+                            assert.equal(res.body[3].name, "testWebsite1");
+                            done();
                         });
-                        done();
-                    });
-            })
-    })
-});
+                });
+        });
+
+        it('updates a website', function testUpdateWebsite(done) {
+            request(server).put('/api/website/123')
+                .send({"_id": "123", "name": "Facebook2", "developerId": "456", "description": "Lorem"})
+                .expect(200)
+                .end((err, res) => {
+                    request(server)
+                        .get('/api/website/123')
+                        .expect(200)
+                        .end((err, res) => {
+                            assert.deepEqual(res.body, {
+                                "_id": "123",
+                                "name": "Facebook2",
+                                "developerId": "456",
+                                "description": "Lorem"
+                            });
+                            done();
+                        });
+                })
+        })
+    });
+})();
