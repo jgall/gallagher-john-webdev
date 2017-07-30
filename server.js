7b48a72e18b73917f319d97ab16845e090a02ca6
Gallagher <jggll@amazon.com>
2017-07-30 14:15:02 -0400

    Improve code styling.

--- server.js
+++ server.js
@@ -1,21 +1,25 @@
-'use strict';
+module.exports = (function() {
+    'use strict';
 
-var express = require('express');
-var app = express();
+    let express = require('express');
+    let app = express();
 
-var bodyParser = require('body-parser');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+    let bodyParser = require('body-parser');
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
 
 // configure a public directory to host static content
-app.use(express.static(__dirname + '/public'));
+    app.use(express.static(__dirname + '/public'));
 
-require("./testMongo/app.js")(app);
+    require("./testMongo/app.js")(app);
 
-require("./assignment/app")(app);
+    require("./assignment/app")(app);
 
-var port = process.env.PORT || 3001;
+    let port = process.env.PORT || 3001;
 
-var server = app.listen(port);
+    let server = app.listen(port);
 
-module.exports = server;
+    return server;
+})();
+
+
