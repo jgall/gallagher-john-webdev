ff25e15bef11abee7d39ead87f2054912613b89e
Gallagher <jggll@amazon.com>
2017-08-02 09:38:30 -0400

    Implement front end angular Flickr Service.

--- public/assignment/services/flickr.service.client.js
+++ public/assignment/services/flickr.service.client.js
@@ -6,17 +6,16 @@
     angular.module('WebAppMaker').factory("FlickrService", FlickrService);
 
     function FlickrService($http) {
-        var key = "your-flickr-key";
-        var secret = "your-flickr-secret";
-        var urlBase = "https://api.flickr.com/services/rest/?method=flickr.photos.search&format=json&api_key=API_KEY&text=TEXT";
+        const key = "0a983ebce36774b4c54d92c9ab41a8d2";
+        const urlBase = "https://api.flickr.com/services/rest/?method=flickr.photos.search&format=json&api_key=API_KEY&text=TEXT";
 
-        let api = {};
+        const api = {"searchPhotos": searchPhotos};
 
         return api;
 
         function searchPhotos(searchTerm) {
             let url = urlBase.replace("API_KEY", key).replace("TEXT", searchTerm);
-            return $http.get(url);
+            return $http.get(url).then(res => res.data);
         }
 
     }
