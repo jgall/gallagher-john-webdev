280ee2274c6753bfd35752fe4ff37c9e1c670a06
Gallagher <jggll@amazon.com>
2017-08-03 15:01:28 -0400

    Add to gitignore.

---
+++ public/project/poc/app.js
@@ -0,0 +1,11 @@
+/**
+ * Created by jggll on 7/17/17.
+ */
+(function() {
+    let app = angular.module("POC",["ngRoute", "ngAnimate"]);
+
+    app.controller("PocController", function($http) {
+        
+    })
+
+})();
