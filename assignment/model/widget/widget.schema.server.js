e41384ff757d875a1c29f67386e8d331b58814d8
Gallagher <jggll@amazon.com>
2017-08-04 07:55:25 -0400

    Fix typo.

--- assignment/model/widget/widget.schema.server.js
+++ assignment/model/widget/widget.schema.server.js
@@ -17,7 +17,7 @@ module.exports = (function () {
         icon: String,
         deletable: Boolean,
         formatted: Boolean,
-        dateCreated: {type: Date, default: Date.now},
+        dateCreated: {type: Date, default: Date.now}
     });
     return schema;
 })();
