3978d1fa66e01e92e7a4a66ccd4b5409d9e8823b
Gallagher <jggll@amazon.com>
2017-08-05 09:30:06 -0400

    Implement widget reordering on mongo model.

--- assignment/model/widget/widget.model.server.js
+++ assignment/model/widget/widget.model.server.js
@@ -21,18 +21,23 @@ module.exports = (function () {
     }
 
     function findAllWidgetsForPage(pageId) {
+        return widgetModel.find({_page: pageId});
     }
 
     function findWidgetById(widgetId) {
+        return widgetModel.findById(widgetId);
     }
 
     function updateWidget(widgetId, widget) {
+        widgetModel.update({_id: widgetId}, {$set: widget});
     }
 
     function deleteWidget(widgetId) {
     }
 
     function reorderWidget(pageId, start, end) {
+        return getPageModelApi().findPageById(pageId).then(page => getPageModelApi()
+            .updatePage(pageId, {widgets: moveInArray(page.widgets, start, end)}))
     }
 
     function deleteWidgetsOfPage(pageId) {
@@ -44,6 +49,17 @@ module.exports = (function () {
             pageModelApi = require("../page/page.model.server");
         }
         return pageModelApi;
+    }
+
+    function moveInArray(arr, old_index, new_index) {
+        if (new_index >= arr.length) {
+            let k = new_index - arr.length;
+            while ((k--) + 1) {
+                arr.push(undefined);
+            }
+        }
+        arr.splice(new_index, 0, arr.splice(old_index, 1)[0]);
+        return arr; // for testing purposes
     }
 
 })();
